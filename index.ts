d497d2d365581999738a0a72f2966d0d8b225797
Phillip Nguyen <png9981@gmail.com>
2024-06-13 19:32:10 +0700

    feat: initial commit to implement task queue

---
+++ index.ts
@@ -0,0 +1,48 @@
+async function processTask(task: any): Promise<any> {
+  // Simulate an asynchronous task
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(task)
+      resolve(task * 2); // Example task: multiply by 2
+    }, 1000);
+  });
+}
+
+async function runTasksWithConcurrencyLimit<T>(
+  data: T[],
+  asyncFunc: (data: T) => Promise<void>,
+  concurrencyLimit: number
+): Promise<void> {
+  let index = 0;
+
+  const taskWrapper = async () => {
+    if (index >= data.length) {
+      return
+    }
+    await asyncFunc(data[index++]);
+    await taskWrapper()
+  }
+
+  const p: Promise<any>[] = []
+  for (let i = 0; i < concurrencyLimit; i++) {
+    p.push(taskWrapper())
+  }
+  await Promise.all(p)
+
+}
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms * 1000));
+
+(async () => {
+  console.time('runTasksWithConcurrencyLimit')
+  await runTasksWithConcurrencyLimit<number>(
+    [1, 7, 2, 3, 5, 4, 6, 8],
+    async (data) => {
+      console.log("start process", data)
+      await sleep(data);
+      console.log('Finish', data);
+    },
+    3
+  )
+  console.timeEnd('runTasksWithConcurrencyLimit')
+})()
